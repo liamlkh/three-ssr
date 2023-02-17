09025528b3630a21e784eabcee64b6988ddf34dd
Liam <>
2023-02-17 16:54:51 -0500

    no message

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 const fs = require('fs');
-const gl = require('gl');
 const http = require('http');
 const THREE = require("three");
 const jimp = require("jimp");
@@ -49,7 +48,7 @@ class App {
     this.camera.position.z = 80;
     this.camera.zoom = 0.9;
     this.clock = new THREE.Clock();
-    this.gl = gl(this.width, this.height, { preserveDrawingBuffer: true }); //headless-gl
+    this.gl = require('gl')(this.width, this.height, { preserveDrawingBuffer: true }); //headless-gl
     this.renderer = new THREE.WebGLRenderer({ context: this.gl });
     this.renderer.setSize(this.width, this.height);
     this.renderer.outputEncoding = THREE.sRGBEncoding;
