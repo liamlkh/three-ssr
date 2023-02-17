4c16301662dd5f2c071a710d4690564478c0bdb3
Liam <>
2023-02-17 14:13:55 -0500

    no message

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-  .use(express.static(path.join(__dirname, '../client')))
+  .use(express.static(path.join(__dirname, '/client')))
   // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
